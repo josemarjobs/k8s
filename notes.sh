1e49df839e43d78b8c4772ea42508fa033bdc778
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2016-12-26 00:02:12 -0800

    Create mypod

--- notes.sh
+++ notes.sh
@@ -2,3 +2,29 @@
 #   -os="linux" \
 #   -arch="amd64" \
 #   -output="dist/{{.OS}}-{{.Arch}}/{{.Dir}}" app
+
+# start minikube
+minikube start --vm-driver=virtualbox
+
+# get information about the cluster
+kubectl cluster-info
+kubectl get cs # component-info
+
+# create components
+kubectl create -f file.yml
+# delete them
+kubectl delete pod name
+kubectl delete svc|service name
+# list pods
+kubectl get pods
+# list services
+kubectl get svc|services
+# describe
+kubectl describe svc name
+kubectl describe pod name
+# scaling
+kubectl scale rc web --replicas=10
+
+# expose
+kubectl expose pod mypod --type=NodePort # to the outside world
+kubectl expose pod mypod --type=ClusterIP # to the cluster
